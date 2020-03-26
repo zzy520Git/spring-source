9bc59136f16e74b12138705946b952cdd2126aaf
zhouzhongyi <zzy520com@qq.com>
2020-03-26 22:39:13 +0800

    spi

--- springmvc-annotation/src/main/java/notes/service/spi/SpiServiceImpl.java
+++ springmvc-annotation/src/main/java/notes/service/spi/SpiServiceImpl.java
@@ -1,6 +1,10 @@
 package notes.service.spi;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 /**
  * Description：
@@ -9,8 +13,14 @@ import lombok.extern.slf4j.Slf4j;
  * @date 2020/3/26 22:00
  */
 @Slf4j
-public class SpiServiceImpl implements SpiService {
+public class SpiServiceImpl implements SpiService, WebApplicationInitializer {
+    //WebApplicationInitializer
     public SpiServiceImpl() {
         log.error("观察spi机制。。。");
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        log.error("观察spi机制onStartup。。。");
     }
 }
