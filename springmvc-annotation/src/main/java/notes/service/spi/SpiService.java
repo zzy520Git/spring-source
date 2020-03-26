a58289054df7287fa4933a997d1542ed3759d9a9
zhouzhongyi <zzy520com@qq.com>
2020-03-26 22:12:22 +0800

    spi还存在问题

---
+++ springmvc-annotation/src/main/java/notes/service/spi/SpiService.java
@@ -0,0 +1,10 @@
+package notes.service.spi;
+
+/**
+ * Description：
+ *
+ * @author zzy-PC
+ * @date 2020/3/26 22:00
+ */
+public interface SpiService {
+}
---
+++ springmvc-annotation/src/main/java/notes/service/spi/SpiServiceImpl.java
@@ -0,0 +1,16 @@
+package notes.service.spi;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Description：
+ *
+ * @author zzy-PC
+ * @date 2020/3/26 22:00
+ */
+@Slf4j
+public class SpiServiceImpl implements SpiService {
+    public SpiServiceImpl() {
+        log.error("观察spi机制。。。");
+    }
+}
