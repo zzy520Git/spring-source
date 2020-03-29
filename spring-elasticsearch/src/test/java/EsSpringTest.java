cdb82eff161e315575382b85651626d864974c3b
zhouzhongyi <zzy520com@qq.com>
2020-03-29 17:43:58 +0800

    es高级查询

--- spring-elasticsearch/src/test/java/EsSpringTest.java
+++ spring-elasticsearch/src/test/java/EsSpringTest.java
@@ -7,6 +7,9 @@ import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Date;
+import java.util.Optional;
+
 /**
  * Description：
  *
@@ -29,6 +32,30 @@ public class EsSpringTest {
         //elasticsearchTemplate.putMapping(Comment.class);
         System.out.println("索引名必须全部小写");
 
+    }
+
+    @Test
+    public void addDocument() {
+        //更新文档等同于添加文档，id相同就行
+        Comment comment = new Comment();
+        comment.setId("1001");
+        comment.setDate(new Date());
+        comment.setContent("你是是电信1001班的某某某吗？in china!");
+        Comment save = commentEsRepository.save(comment);
+        //输出true
+        System.out.println(save==comment);
+    }
+
+    @Test
+    public void delDocument() {
+        commentEsRepository.deleteById("1001");
+    }
+
+    @Test
+    public void selectDocument() {
+        Optional<Comment> optionalComment = commentEsRepository.findById("1001");
+        Comment comment = optionalComment.get();
+        System.out.println(comment);
     }
 
 }
